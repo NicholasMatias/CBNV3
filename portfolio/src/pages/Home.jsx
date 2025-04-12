ae7cccd38b972d2ce16789dcd3df722cc587fb5d
NicholasMatias <n-r-matias@outlook.com>
2025-04-11 21:29:35 -0500

    added email form, may change from web3

---
+++ portfolio/src/pages/Home.jsx
@@ -0,0 +1,19 @@
+import Intro from '../components/Intro';
+import Education from '../components/Education';
+import Leadership from '../components/Leadership';
+import Skills from '../components/Skills';
+import Hobbies from '../components/Hobbies';
+
+const Home = () => {
+  return (
+    <main>
+      <Intro />
+      <Education />
+      <Leadership />
+      <Skills />
+      <Hobbies />
+    </main>
+  );
+};
+
+export default Home; 
